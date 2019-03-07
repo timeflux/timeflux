4fe3dcc72a6532e4d908b5367f1f2c904559c9d4
mesca <github@clisson.org>
2019-03-07 18:59:55 +0100

    Remove an unused exception

--- timeflux/core/exceptions.py
+++ timeflux/core/exceptions.py
@@ -3,15 +3,8 @@
 class TimefluxException(Exception):
     """Generic Exception."""
 
-class WorkerTerminated(TimefluxException):
-    """Raised when a worker died prematurely."""
-    def __init__(self, message, pid, *args):
-        self.message = message
-        self.pid = pid
-        super(WorkerTerminated, self).__init__(message, pid, *args)
-
 class WorkerInterrupt(TimefluxException):
     """Raised when a process stops prematurely."""
     def __init__(self, message, *args):
         self.message = message
-        super(WorkerInterrupt, self).__init__(message, *args)
+        super().__init__(message, *args)
