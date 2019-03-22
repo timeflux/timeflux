2d601881ca4a47eee795e3df44e75f317c3fd721
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-03-22 23:01:09 +0100

    Uptodate with master branch to allow pull-request

--- timeflux/core/node.py
+++ timeflux/core/node.py
@@ -1,10 +1,20 @@
 """Node base class."""
 
 import re
+import logging
 from abc import ABC, abstractmethod
 from timeflux.core.io import Port
 
 class Node(ABC):
+
+
+    def __new__(cls, *args, **kwargs):
+        """Create instance and initialize the logger."""
+
+        instance = super().__new__(cls)
+        instance.logger = logging.getLogger(__name__ + '.' + cls.__name__)
+        return instance
+
 
     def __init__(self):
         """Instantiate the node."""
@@ -14,14 +24,11 @@ class Node(ABC):
 
     def __getattr__(self, name):
         """Create input and output ports on the fly.
-
         Args:
             name (string): The name of the port, prefixed with `i_` (input) or `o_` (output).
                            Default ports are also allowed (`i` or `o`).
-
         Returns:
             Port: The newly created port.
-
         """
 
         if name == 'i' or name.startswith('i_') or name == 'o' or name.startswith('o_'):
@@ -33,21 +40,16 @@ class Node(ABC):
 
     def iterate(self, name='*'):
         """Iterate through ports.
-
         If ``name`` ends with the globbing character (`*`), the generator iterates through all existing
         ports matching that pattern. Otherwise, only one port is returned. If it does not already exist,
         it is automatically created.
-
         Args:
             name (string): The matching pattern.
-
         Yields:
             (tupple): A tupple containing:
-
             * name (`string`): The full port name.
             * suffix (`string`): The part of the name matching the globbing character.
             * port (`Port`): The port object.
-
         """
 
         if not self.ports: self.ports = {}
@@ -63,11 +65,9 @@ class Node(ABC):
 
     def clear(self):
         """Reset all ports.
-
         It is assumed that numbered ports (i.e. those with a name ending with an underscore followed by numbers)
         are temporary and must be completely removed. All other ports are simply emptied to avoid the cost of
         reinstanciating a new `Port` object before each update.
-
         """
 
         if not self._re_dynamic_port:
