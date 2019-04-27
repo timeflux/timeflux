b966f1cf4c052c5383f3f081ddad6a32274cece6
mesca <github@clisson.org>
2019-04-27 21:13:09 +0200

    Look for .env file in current directory

--- timeflux/timeflux.py
+++ timeflux/timeflux.py
@@ -32,7 +32,7 @@ def _args():
     parser = ArgumentParser()
     parser.add_argument('-v', '--version', action='version', version='Timeflux %s' % __version__)
     parser.add_argument('-d', '--debug', default=False, action='store_true', help='enable debug messages')
-    parser.add_argument('-e', '--env', help='path to an environment file')
+    parser.add_argument('-e', '--env', default='./.env', help='path to an environment file')
     parser.add_argument('app', help='path to the YAML or JSON application file')
     args = parser.parse_args()
     return args
