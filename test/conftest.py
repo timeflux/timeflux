7e606286fa2f9e5802004faa99e1a7272d42ac0e
mesca <github@clisson.org>
2019-05-02 13:20:43 +0200

    Remove pytest_namespace hook

--- test/conftest.py
+++ test/conftest.py
@@ -7,8 +7,6 @@ import pytest
 PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, PATH  + '/../')
 
-def pytest_namespace():
-    return {'path': PATH}
-
 def pytest_configure(config):
+    pytest.path = PATH
     config.addinivalue_line('filterwarnings', 'ignore:.*ABCs.*:DeprecationWarning')
