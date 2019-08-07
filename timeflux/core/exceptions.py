b563cb2f22bffa9a008f5d0871f3cfb491cb42ae
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-07 19:05:27 +0200

    Add an Exception specific to a node failure.

--- timeflux/core/exceptions.py
+++ timeflux/core/exceptions.py
@@ -2,29 +2,46 @@
 
 __all__ = ['WorkerInterrupt', 'WorkerLoadError', 'GraphDuplicateNode', 'GraphUndefinedNode']
 
+
 class TimefluxException(Exception):
     """Generic Exception."""
 
+
 class GraphDuplicateNode(TimefluxException):
     """Raised when a duplicate node is found."""
+
     def __init__(self, message, *args):
         self.message = message
         super().__init__(message, *args)
+
 
 class GraphUndefinedNode(TimefluxException):
     """Raised when an undefined node is found in edges."""
+
     def __init__(self, message, *args):
         self.message = message
         super().__init__(message, *args)
+
 
 class WorkerLoadError(TimefluxException):
     """Raised when a worker cannot be loaded."""
+
     def __init__(self, message, *args):
         self.message = message
         super().__init__(message, *args)
 
+
 class WorkerInterrupt(TimefluxException):
     """Raised when a process stops prematurely."""
+
     def __init__(self, message='Interrupting', *args):
+        self.message = message
+        super().__init__(message, *args)
+
+
+class NodeValueError(TimefluxException):
+    """Raised when a node received invalid data"""
+
+    def __init__(self, message, *args):
         self.message = message
         super().__init__(message, *args)
