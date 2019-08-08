4528886b67b67afba6dfe2cdb4853bef7627d5b5
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-08 13:09:21 +0200

    Add a dejitter module with two Nodes: - basic snap using pandas round method - interpolation using pandas interpolate method + Associated unit test

---
+++ test/test_node_dejitter.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import pytest
+from timeflux.core.exceptions import NodeValueError
+from timeflux.helpers.testing import DummyData, Looper
+from timeflux.nodes.dejitter import Round, Interpolate
+
+rate = 10
+
+dummy_data_with_jitter = DummyData(rate=rate, jitter=.05)
+
+num_cols = 5
+dummy_data_no_jitter = DummyData(rate=rate, jitter=0.0, cols=[f'ch{k}' for k in range(num_cols)])
+
+dummy_data_not_monotonic = DummyData(rate=rate, jitter=.05, num_rows=100)
+# swap rows 51 and 52 to have a DataFrame with not monotonic index
+dummy_data_not_monotonic._data = dummy_data_not_monotonic._data.iloc[
+    list(np.arange(0, 50)) + [52, 51] + list(np.arange(52, 100))]
+assert ~ dummy_data_not_monotonic._data.index.is_monotonic
+
+
+def test_round_on_data_with_jitter():
+    data = dummy_data_with_jitter
+    data.reset()
+    node = Round(rate=rate)
+    node.i.data = data.next(6)
+    node.update()
+
+    expected_data = pd.DataFrame(
+        [
+            [0.185133, 0.541901, 0.872946, 0.732225, 0.806561],
+            [0.658783, 0.692277, 0.849196, 0.249668, 0.489425],
+            [0.221209, 0.987668, 0.944059, 0.039427, 0.705575],
+            [0.925248, 0.180575, 0.567945, 0.915488, 0.033946],
+            [0.69742, 0.297349, 0.924396, 0.971058, 0.944266],
+            [0.474214, 0.862043, 0.844549, 0.3191, 0.828915]
+        ],
+        [
+            pd.Timestamp('2018-01-01 00:00:00.000'),
+            pd.Timestamp('2018-01-01 00:00:00.100'),
+            pd.Timestamp('2018-01-01 00:00:00.200'),
+            pd.Timestamp('2018-01-01 00:00:00.300'),
+            pd.Timestamp('2018-01-01 00:00:00.400'),
+            pd.Timestamp('2018-01-01 00:00:00.500'),
+        ]
+    )
+    pd.testing.assert_frame_equal(expected_data, node.o.data)
+
+
+def test_round_on_data_no_jitter():
+    data = dummy_data_no_jitter
+    data.reset()
+    node = Round(rate=rate)
+    looper = Looper(data, node)
+    dejittered_data, _ = looper.run(chunk_size=8)
+    pd.testing.assert_frame_equal(dejittered_data, data._data)
+
+
+def test_interpolate_on_data_no_jitter():
+    data = dummy_data_no_jitter
+    data.reset()
+    interpolate = Interpolate(rate=rate, method='linear')
+    looper = Looper(data, interpolate)
+    dejittered_data, _ = looper.run(chunk_size=8)
+    pd.testing.assert_frame_equal(dejittered_data, data._data.iloc[:-1])
+
+
+def test_interpolate_on_linear_data():
+    linear_data = pd.DataFrame(
+        [
+            [0.1],
+            [0.2],
+            [0.3],
+            [0.4],
+            [0.5],
+            [0.6],
+            [0.7],
+            [0.8],
+            [0.9],
+
+        ],
+        [
+            pd.Timestamp('2018-01-01 00:00:00.000'),
+            pd.Timestamp('2018-01-01 00:00:00.100'),
+            pd.Timestamp('2018-01-01 00:00:00.200'),
+            pd.Timestamp('2018-01-01 00:00:00.300'),
+            pd.Timestamp('2018-01-01 00:00:00.400'),
+            pd.Timestamp('2018-01-01 00:00:00.500'),
+            pd.Timestamp('2018-01-01 00:00:00.600'),
+            pd.Timestamp('2018-01-01 00:00:00.700'),
+            pd.Timestamp('2018-01-01 00:00:00.800'),
+
+        ],
+        ['A']
+    )
+    linear_data_with_nan = linear_data.copy()
+    linear_data_with_nan.iloc[[2, 3, 5]] = np.NaN
+
+    interpolate = Interpolate(rate=10, method='linear')
+    interpolate.i.data = linear_data_with_nan
+    interpolate.update()
+    pd.testing.assert_frame_equal(interpolate.o.data, linear_data.iloc[:-1])
+
+
+def test_data_not_monotonic():
+    data = dummy_data_not_monotonic
+    node = Interpolate(rate=rate)
+    looper = Looper(data, node)
+    with pytest.raises(NodeValueError):
+        _, _ = looper.run(chunk_size=8)
---
+++ timeflux/nodes/dejitter.py
@@ -0,0 +1,135 @@
+"""timeflux.nodes.dejitter """
+
+import numpy as np
+import pandas as pd
+
+from timeflux.core.node import Node
+from timeflux.core.exceptions import NodeValueError
+
+
+# from timeflux.helpers.clock import *
+
+class Round(Node):
+    """  Snap time stamps to nearest occurring frequency.
+
+    Attributes:
+       i (Port): Default input, expects DataFrame and meta.
+       o (Port): Default output, provides DataArray and meta.
+    Args:
+        rate (float|None): (optional) nominal sampling frequency of the data, to round
+            the timestamps to (in Hz). If None, the rate will be get from the meta
+            of the input port.
+    """
+
+    def __init__(self, rate=None):
+        self._rate = rate
+
+    def update(self):
+
+        # copy the meta and the data
+        self.o = self.i
+
+        # if the rate has not been set in the constructor, get it from the meta
+        if self._rate is None:
+            self._rate = self.i.meta.get('rate')
+
+        # When we have not received data, there is nothing to do
+        if self._rate is None or self.i.data is None or self.i.data.empty:
+            return
+
+        # At this point, we are sure that we have some data to process
+        self.o.data.index = self.o.data.index.round(str(1 / self._rate) + "S")
+
+
+class Interpolate(Node):
+    """ Dejitter data with values interpolation
+
+     This nodes continuously buffers a small amount of data to allow for
+     interpolating either missing samples.
+     The output data is resampled at a fixed rate.
+     The interpolation is performed by pandas methods.
+
+    Attributes:
+       i (Port): Default input, expects DataFrame and meta.
+       o (Port): Default output, provides DataArray and meta.
+
+    Args:
+        rate (float|None): (optional) nominal sampling frequency of the data, to round
+            the timestamps to (in Hz). If None, the rate will be get from the meta
+            of the input port.
+        method: interpolation method. See the pandas.DataFrame.interpolate documentation.
+        n_min: minimum number of samples to perform the interpolation.
+        n_max: number of samples to keep in the buffer.
+
+    Notes:
+        Computation cost mainly depends on the window size and the estimation is
+        performed in the main thread. Hence, the user should be careful on the
+        computation duration.
+    """
+
+    def __init__(self, rate=None, method="cubic", n_min=3, n_max=10):
+
+        self._rate = rate
+        if self._rate is not None:
+            self._set_timedelta()
+
+        self._method = method  # {‘linear’, ‘time’, ‘index’, ‘values’, ‘nearest’, ‘zero’, ‘slinear’, ‘quadratic’, ‘cubic’, ‘barycentric’, ‘krogh’, ‘polynomial’, ‘spline’, ‘piecewise_polynomial’, ‘from_derivatives’, ‘pchip’, ‘akima’}
+        self._n_min = n_min
+        self._n_max = n_max
+        self._last_datetime = None
+
+    def _set_timedelta(self):
+        self._timespan = 1 / self._rate
+        self._timedelta = pd.to_timedelta(self._timespan, 's')  # sampling period of the interpolated signal
+
+    def update(self):
+
+        self.o.meta = self.i.meta
+
+        # if the rate has not been set in the constructor, get it from the meta
+        if self._rate is None:
+            self._rate = self.i.meta.get('rate')
+            self._set_timedelta()
+
+        if self.i.data is None or self.i.data.empty:
+            return
+
+        # initialize the first datetime index.
+        if self._last_datetime is None:
+            self._last_datetime = self.i.data.index.round(str(self._timespan) + 'S')[0]
+            self._buffer = pd.DataFrame()
+            self._times = np.arange(self._last_datetime,
+                                    self.i.data.index[-1],
+                                    self._timedelta,
+                                    dtype="datetime64[us]")
+        else:
+            self._times = np.arange(self._last_datetime + self._timedelta,
+                                    self.i.data.index[-1],
+                                    self._timedelta,
+                                    dtype="datetime64[us]")
+
+        # interpolate
+        self._interpolate()
+
+    def _drop_duplicates(self, data):
+        return data.loc[~data.index.duplicated(keep='first')]
+
+    def _interpolate(self):
+        # interpolate current chunk
+        self._buffer = self._buffer.append(self.i.data, sort=True)  # append last sample be able to interpolate
+
+        if not self._buffer.index.is_monotonic:
+            raise NodeValueError('Data index should be strictly monotonic')
+
+        data_to_interpolate = self._buffer.append(pd.DataFrame(index=self._times),
+                                                  sort=True)
+        data_to_interpolate = self._drop_duplicates(data_to_interpolate).sort_index()
+
+        if (self._buffer.notnull().sum(axis=0) > self._n_min).all():
+            self.o.data = (data_to_interpolate.interpolate(axis=0, method=self._method)
+                           .reindex(self._times).loc[self._last_datetime:]
+                           .dropna(axis=0, how="any"))
+
+            if not self.o.data.empty:
+                self._last_datetime = self.o.data.index[-1]
+                self._buffer = self._buffer.tail(self._n_max)
