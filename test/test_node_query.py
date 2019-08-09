e580ed6cb833eb92d7efa8ca0afe3ea12fb7254e
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-09 14:25:15 +0200

    Clean-up the code.

--- test/test_node_query.py
+++ test/test_node_query.py
@@ -1,62 +1,83 @@
 """Tests for query nodes"""
 
-import pytest
 import pandas as pd
-import numpy as np
-from timeflux.core.registry import Registry
-import helpers
-import xarray as xr
-
-Registry.cycle_start = 0
-Registry.rate = 1
-
+import pytest
+from timeflux.helpers.testing import DummyData
 from timeflux.nodes.query import *
 
-fs=10
-data = helpers.DummyData( rate=fs, jitter=.05, num_cols=6)
+fs = 10
+data = DummyData(rate=fs, jitter=.05, num_cols=6)
 all_data = data._data
-chunk_size = 3
-
-
 
 
 def test_locquery():
     data.reset()
     data._data.columns = ['A', 'B', 'C', 'D', 'E', 'F']
-    node = LocQuery(key=['A', 'E'], axis = 1)
-    node.i.data = data.next(chunk_size)
+    node = LocQuery(key=('A', 'E'), axis=1)
+    node.i.data = data.next(3)
     node.update()
-    pd.testing.assert_frame_equal(node.o.data, all_data.loc[:, ["A", "E"]][:chunk_size] )
+    expected_data = pd.DataFrame(
+        [
+            [0.185133, 0.806561],
+            [0.692277, 0.221209],
+            [0.944059, 0.180575]
+        ],
+        [
+            pd.Timestamp('2017-12-31 23:59:59.998745401'),
+            pd.Timestamp('2018-01-01 00:00:00.104507143'),
+            pd.Timestamp('2018-01-01 00:00:00.202319939'),
+
+        ],
+        ['A', 'E']
+    )
+    pd.testing.assert_frame_equal(node.o.data, expected_data)
 
     # test query with wrong key: "R" not in the input columns
-    with pytest.raises(ValueError):
-        node = LocQuery(key=['R'], axis=1)
+    with pytest.raises(KeyError):
+        node = LocQuery(key='R', axis=1)
         node.i.data = data.next()
         node.update()
 
 
 def test_xsquery():
     data.reset()
-    data._data.columns = pd.MultiIndex.from_product([['A', 'B'], [1,2,3]], names=["first", "second"])
-    node = XsQuery(key=(1, 'B'), axis = 1, level= ["second", "first"], drop_level=False)
-    node.i.data = data.next(chunk_size)
+    data._data.columns = pd.MultiIndex.from_product([['A', 'B'], [1, 2, 3]], names=['first', 'second'])
+    node = XsQuery(key=(1, 'B'), axis=1, level=['second', 'first'], drop_level=False)
+    node.i.data = data.next(3)
     node.update()
-    pd.testing.assert_frame_equal(node.o.data, all_data.loc[:, [("B", 1)]][:chunk_size])
+    expected_data = pd.DataFrame(
+        [[0.732225],
+         [0.489425],
+         [0.925248]],
+        [
+            pd.Timestamp('2017-12-31 23:59:59.998745401'),
+            pd.Timestamp('2018-01-01 00:00:00.104507143'),
+            pd.Timestamp('2018-01-01 00:00:00.202319939'),
 
-    # test query with wrong key: "R" not in the input columns
-    with pytest.raises(ValueError):
-        node = LocQuery(key=['R'], axis=1)
-        node.i.data = data.next()
-        node.update()
+        ],
+        pd.MultiIndex.from_product([['B'], [1]], names=['first', 'second'])
+    )
+    pd.testing.assert_frame_equal(node.o.data, expected_data)
+
 
 def test_selectrange():
     data.reset()
-    node = SelectRange(ranges={"second": [1,1.5]}, axis= 1, inclusive=True)
-    data._data.columns = pd.MultiIndex.from_product([['A', 'B'], [1.3,1.6, 1.9]], names=["first", "second"])
-    node.i.data = data.next(chunk_size)
+    node = SelectRange(ranges={'second': [1, 1.5]}, axis=1, inclusive=True)
+    data._data.columns = pd.MultiIndex.from_product([['A', 'B'], [1.3, 1.6, 1.9]],
+                                                    names=['first', 'second'])
+    node.i.data = data.next(3)
     node.update()
-    pd.testing.assert_frame_equal(node.o.data, all_data.xs((1.3), level= "second", axis=1, drop_level=False)[:chunk_size])
+    expected_data = pd.DataFrame(
+        [[0.185133, 0.732225],
+         [0.692277, 0.489425],
+         [0.944059, 0.925248]],
+        [
+            pd.Timestamp('2017-12-31 23:59:59.998745401'),
+            pd.Timestamp('2018-01-01 00:00:00.104507143'),
+            pd.Timestamp('2018-01-01 00:00:00.202319939'),
 
+        ],
+        pd.MultiIndex.from_product([['A', 'B'], [1.3]], names=['first', 'second'])
+    )
 
-
-
+    pd.testing.assert_frame_equal(node.o.data, expected_data)
