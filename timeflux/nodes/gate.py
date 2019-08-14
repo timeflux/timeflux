bc84c720056650d66d0306dcdd594726f164c9dc
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-14 12:12:20 +0200

    Truncate data not required (add parameter `truncate` in node gate)

--- timeflux/nodes/gate.py
+++ timeflux/nodes/gate.py
@@ -33,54 +33,59 @@ class Gate(Node):
 
     """
 
-    def __init__(self, event_opens, event_closes, event_label='label'):
+    def __init__(self, event_opens, event_closes, event_label='label', truncate=False):
 
         self._event_label = event_label
         self._event_opens = event_opens
         self._event_closes = event_closes
+        self._truncate = truncate
         self._reset()
 
     def update(self):
         # Iter over events to match the opening/closing trigger.
+
         if self.i_events.ready():
             for index, row in self.i_events.data.iterrows():
                 if row[self._event_label] == self._trigger:
+                    self.logger.debug(f'Gate received {self._trigger}.')
                     self._next()
                     # keep track of opening/closing of the gate
                     self._times.append(index)
-                    self._update()
-        else:
-            self._update()
+        self._update()
 
     def _update(self):
         # if the gate is either open or closing, truncate and forward the data,
         # if the gate is closing, reset status and trigger iterator,
         # else (gate is closed), return
+
         if self._status == 'open':
             self.o = self.i
             self.o.meta['gate_status'] = self._status
-            if isinstance(self.o.data, xr.DataArray):
-                self.o.data = self.o.data.sel({'time': slice(self._times[0], self.o.data.time[-1])})
-            else:  # isinstance(self.o.data,pd.DataFrame)
-                # truncate the data after opening time
-                self.o.data = self.o.data[self._times[0]:]
+            if self._truncate and self.o.ready():
+                if isinstance(self.o.data, xr.DataArray):
+                    self.o.data = self.o.data.sel({'time': slice(self._times[0], self.o.data.time[-1])})
+                else:  # isinstance(self.o.data,pd.DataFrame)
+                    # truncate the data after opening time
+                    self.o.data = self.o.data[self._times[0]:]
         elif self._status == 'closing':
             self.o = self.i
+            self.o.meta.update({'gate_status': 'closed', 'gate_times': self._times})
             # truncate the data between opening and closing times
-            if isinstance(self.o.data, xr.DataArray):
-                self.o.data = self.o.data.sel({'time': slice(self._times[0], self._times[1])})
-            else:  # isinstance(self.o.data,pd.DataFrame)
-                self.o.data = self.o.data[self._times[0]:self._times[1]]
-
-            self.o.meta = {'gate_status': 'closed', 'gate_times': self._times}
+            if self._truncate and self.o.ready():
+                if isinstance(self.o.data, xr.DataArray):
+                    self.o.data = self.o.data.sel({'time': slice(self._times[0], self._times[1])})
+                else:  # isinstance(self.o.data,pd.DataFrame)
+                    self.o.data = self.o.data[self._times[0]:self._times[1]]
             self._reset()
         else:  # self._status == 'closed'
-            return
+            self.o.data = None
+            self.o.meta = {'gate_status': 'closed'}
 
     def _next(self):
         # iterates trigger (expected event) and status (defining the mode of the node)
-        self._trigger = next(self._trigger_iterator)
         self._status = next(self._status_iterator)
+        self._trigger = next(self._trigger_iterator)
+        self.logger.debug(f'Gate is {self._status}.')
 
     def _reset(self):
         # Reset iterator states
