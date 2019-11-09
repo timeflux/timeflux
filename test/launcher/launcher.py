fed71e71b7b03a4748afd5316d39488b2106eae0
mesca <github@clisson.org>
2019-11-09 22:02:35 +0100

    Support Windows better

--- test/launcher/launcher.py
+++ test/launcher/launcher.py
@@ -1,89 +1,100 @@
 import sys
 import glob
 import re
+import signal
 import tkinter as tk
 from tkinter import ttk, messagebox, scrolledtext
 from subprocess import Popen, PIPE, STDOUT
 from threading  import Thread, enumerate
 from queue import Queue, Empty
 
-running = False
+class Launcher():
 
-q = Queue()
-ansi_escape = re.compile(r'''
-    \x1B    # ESC
-    [@-_]   # 7-bit C1 Fe
-    [0-?]*  # Parameter bytes
-    [ -/]*  # Intermediate bytes
-    [@-~]   # Final byte
-''', re.VERBOSE)
+    def __init__(self):
+        self.running = False
+        self.queue = Queue()
+        self.ansi_escape = re.compile(r'''
+            \x1B    # ESC
+            [@-_]   # 7-bit C1 Fe
+            [0-?]*  # Parameter bytes
+            [ -/]*  # Intermediate bytes
+            [@-~]   # Final byte
+        ''', re.VERBOSE)
+        if sys.platform == 'win32':
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+        self._init_gui()
 
-def on_start(event):
-    global running
-    button_start['state'] = tk.DISABLED
-    command = ['python', '-m', 'timeflux.helpers.handler', 'launch', 'timeflux', '-d', dropdown.get()]
-    close_fds = 'posix' in sys.builtin_module_names
-    p = Popen(command, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=close_fds, text=True)
-    t = Thread(target=read_stream, args=(p.stdout, q))
-    t.start()
-    running = True
-    output.delete('1.0', tk.END)
-    button_stop['state'] = tk.NORMAL
+    def start(self):
+        self._check_status()
+        self._read_queue()
+        self.window.mainloop()
 
-def on_stop(event):
-    button_stop['state'] = tk.DISABLED
-    Popen(['python', '-m', 'timeflux.helpers.handler', 'terminate'])
+    def _init_gui(self):
+        self.window = tk.Tk()
+        self.window.geometry('800x400')
+        self.window.title('Launcher')
+        frame_top = tk.Frame(bd=1, relief=tk.RIDGE)
+        frame_bottom = tk.Frame(bd=1, relief=tk.RIDGE)
+        self.dropdown = ttk.Combobox(frame_top, values=glob.glob('*.yaml'))
+        self.dropdown.current(0)
+        self.button_start = tk.Button(frame_top, text='Start', fg='green')
+        self.button_stop = tk.Button(frame_top, text='Stop', fg='red', state=tk.DISABLED)
+        self.output = scrolledtext.ScrolledText(frame_bottom)
+        frame_top.pack(fill=tk.BOTH, padx=5, pady=5)
+        frame_bottom.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+        self.dropdown.pack(padx=5, pady=5, fill=tk.X, side=tk.LEFT)
+        self.button_start.pack(padx=5, pady=5, side=tk.LEFT)
+        self.button_stop.pack(padx=5, pady=5, side=tk.LEFT)
+        self.output.pack(fill=tk.BOTH, expand=True)
+        self.button_start.bind('<Button-1>', self._on_start)
+        self.button_stop.bind('<Button-1>', self._on_stop)
+        self.window.protocol('WM_DELETE_WINDOW', self._on_closing)
 
-def on_closing():
-    if running:
-        messagebox.showwarning('Warning', 'A Timeflux instance is running. Please close it before quitting.')
-    else:
-        window.destroy()
+    def _on_start(self, event):
+        if self.button_start['state'] == tk.DISABLED: return
+        command = ['python', '-m', 'timeflux.helpers.handler', 'launch', 'timeflux', '-d', self.dropdown.get()]
+        p = Popen(command, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=True, text=True)
+        t = Thread(target=self._read_stream, args=(p.stdout, self.queue))
+        t.start()
+        self.running = True
+        self.output.delete('1.0', tk.END)
+        self.button_start['state'] = tk.DISABLED
+        self.button_stop['state'] = tk.NORMAL
 
-def read_stream(out, queue):
-    global running
-    for line in iter(out.readline, b''):
-        queue.put(line)
-        if ansi_escape.sub('', line).endswith('Terminated\n'):
-            break;
-    out.close()
-    running = False
-    button_start['state'] = tk.NORMAL
+    def _on_stop(self, event):
+        if self.button_stop['state'] == tk.DISABLED: return
+        command = ['python', '-m', 'timeflux.helpers.handler', 'terminate']
+        Popen(command)
 
-def read_queue():
-    try:
-        line = q.get_nowait()
-        print(line, end = '')
-        output.insert(tk.END, ansi_escape.sub('', line))
-    except:
-        pass
-    window.after(100, read_queue)
+    def _on_closing(self):
+        if self.running:
+            messagebox.showwarning('Warning', 'A Timeflux instance is running. Please stop it before quitting.')
+        else:
+            self.window.destroy()
+
+    def _read_stream(self, out, queue):
+        for line in iter(out.readline, b''):
+            queue.put(line)
+            if self.ansi_escape.sub('', line).endswith('Terminated\n'):
+                break;
+        out.close()
+        self.running = False
+
+    def _read_queue(self):
+        try:
+            line = self.queue.get_nowait()
+            print(line, end = '')
+            self.output.insert(tk.END, self.ansi_escape.sub('', line))
+        except:
+            pass
+        self.window.after(100, self._read_queue)
+
+    def _check_status(self):
+        if self.running == False:
+            self.button_stop['state'] = tk.DISABLED
+            self.button_start['state'] = tk.NORMAL
+        self.window.after(100, self._check_status)
 
 
-window = tk.Tk()
-window.geometry('800x400')
-window.title('Launcher')
-
-frame_top = tk.Frame(bd=1, relief=tk.RIDGE)
-frame_bottom = tk.Frame(bd=1, relief=tk.RIDGE)
-
-dropdown = ttk.Combobox(frame_top, values=glob.glob('*.yaml'))
-dropdown.current(0)
-button_start = tk.Button(frame_top, text='Start', fg='green')
-button_stop = tk.Button(frame_top, text='Stop', fg='red', state=tk.DISABLED)
-output = scrolledtext.ScrolledText(frame_bottom)
-
-frame_top.pack(fill=tk.BOTH, padx=5, pady=5)
-frame_bottom.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
-dropdown.pack(padx=5, pady=5, fill=tk.X, side=tk.LEFT)
-button_start.pack(padx=5, pady=5, side=tk.LEFT)
-button_stop.pack(padx=5, pady=5, side=tk.LEFT)
-output.pack(fill=tk.BOTH, expand=True)
-
-button_start.bind('<Button-1>', on_start)
-button_stop.bind('<Button-1>', on_stop)
-window.protocol('WM_DELETE_WINDOW', on_closing)
-
-read_queue()
-
-window.mainloop()
+if __name__ == '__main__':
+    Launcher().start()
