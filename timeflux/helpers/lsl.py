06760309905c6747cad8dccad2221ec92f8f0c8b
mesca <github@clisson.org>
2019-11-19 20:55:33 +0100

    Add a helper to enumerate LSL streams

---
+++ timeflux/helpers/lsl.py
@@ -0,0 +1,11 @@
+"""Enumerate LSL streams."""
+
+from pylsl import resolve_streams
+
+def enumerate():
+    streams = resolve_streams()
+    for stream in streams:
+        print(f'name: {stream.name()}, type: {stream.type()}, source_id: {stream.source_id()}')
+
+if __name__ == '__main__':
+    enumerate()
