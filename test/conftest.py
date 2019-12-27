85993921d7819ed7cd6c9d459b12261a0333ef68
mesca <github@clisson.org>
2019-12-27 16:21:51 +0100

    Add random fixture

--- test/conftest.py
+++ test/conftest.py
@@ -3,10 +3,17 @@
 import os
 import sys
 import pytest
+import numpy as np
+import random as rand
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PATH  + '/../')
+sys.path.insert(0, os.path.join(PATH, '../'))
 
 def pytest_configure(config):
     pytest.path = PATH
     config.addinivalue_line('filterwarnings', 'ignore:.*ABCs.*:DeprecationWarning')
+
+@pytest.fixture(scope='module')
+def random():
+    rand.seed(0)
+    np.random.seed(0)
