6a9092d948e3d874c6b675d4cfcd8b69067ca337
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-01-16 12:18:13 +0100

    Fix overlapping windows, warn when data are lost and add unit tests

--- timeflux/nodes/window.py
+++ timeflux/nodes/window.py
@@ -12,7 +12,7 @@ class Window(Node):
 
         Args:
             length (float): The length of the window, in seconds.
-            step (float): The sliding step, in seconds.
+            step (float): The minimal sliding step, in seconds.
                 If None (the default), the step will be set to the window duration.
                 If 0, the data will be sent as soon as it is available.
         """
@@ -21,7 +21,7 @@ class Window(Node):
         self._length = pd.Timedelta(seconds=length)
         self._step = pd.Timedelta(seconds=step)
         self._buffer = None
-        self._updated = pd.Timestamp(0)
+        self._overlap = self._step < self._length  # whether epoch should be overlapping
 
     def update(self):
         # Return immediately if we don't have any data
@@ -30,6 +30,7 @@ class Window(Node):
         # Append new data
         if self._buffer is None:
             self._buffer = self.i.data
+            self._updated = self.i.data.index[0]
         else:
             self._buffer = self._buffer.append(self.i.data)
         # Time range
@@ -39,8 +40,12 @@ class Window(Node):
         if self._buffer.index[0] <= low:
             # Step
             if high - self._updated >= self._step:
+                actual_step = (high - self._updated)
+                if self._overlap and (actual_step > self._step):
+                    self.logger.warning(f'Received to much data to provide requested overlapping windows. '
+                                        f'Step is of {actual_step} instead of {self._step}.')
                 # Clear old data
-                self._buffer = self._buffer[low:]
+                self._buffer = self._buffer[self._buffer.index > low]  # self._buffer[low:]
                 # Remember the last time the buffer was updated
                 self._updated = high
                 # Output
