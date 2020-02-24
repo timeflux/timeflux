2d74dcc8336cebd4938ee1f039f361429ca98a6c
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-02-24 16:20:31 +0100

    Allow choosing the axis amongst which to reduce/expand the data

--- timeflux/estimators/transformers/shape.py
+++ timeflux/estimators/transformers/shape.py
@@ -1,20 +1,25 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 
+
 class Expand(BaseEstimator, TransformerMixin):
+    def __init__(self, axis=0):
+        self._axis = axis
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         X = np.asarray(X)
-        return np.expand_dims(X, axis=0)
+        return np.expand_dims(X, axis=self._axis)
 
     def fit_transform(self, X, y=None):
         return self.fit(X).transform(X)
 
 
 class Reduce(BaseEstimator, TransformerMixin):
+    def __init__(self, axis=0):
+        self._axis = axis
 
     def fit(self, X, y=None):
         return self
@@ -22,7 +27,7 @@ class Reduce(BaseEstimator, TransformerMixin):
     def transform(self, X):
         X = np.asarray(X)
         if X.ndim < 3: return X
-        return np.squeeze(X, axis=0)
+        return np.squeeze(X, axis=self._axis)
 
     def fit_transform(self, X, y=None):
         return self.fit(X).transform(X)
