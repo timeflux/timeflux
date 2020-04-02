9f00b71c5d3bcfc8f7ca557e3e09833cc1e3e094
mesca <github@clisson.org>
2020-04-02 21:05:27 +0200

    Provide a simple helper to enumerate groups in a HDF5 file

---
+++ timeflux/helpers/hdf5.py
@@ -0,0 +1,14 @@
+"""Enumerate groups in a HFD5 file."""
+
+import sys
+import tables
+
+def info(fname):
+    f = tables.open_file(fname, 'r')
+    print(f)
+    f.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1: sys.exit()
+    fname = sys.argv[1]
+    info(fname)
