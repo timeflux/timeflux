1fe100436ba4fedc943856e9aa90d4d44c00bc15
mesca <github@clisson.org>
2020-04-23 23:34:58 +0200

    Get full version

--- timeflux/__init__.py
+++ timeflux/__init__.py
@@ -6,4 +6,4 @@ os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
 # Versinoning
 from pbr.version import VersionInfo
-__version__ = VersionInfo(__name__).version_string()
+__version__ = VersionInfo(__name__).release_string()
