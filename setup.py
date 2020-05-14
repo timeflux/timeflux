9fe0d7718738ace03f7210288239dbc87a0740ea
mesca <github@clisson.org>
2020-05-14 20:21:30 +0200

    Build with PEP517

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from setuptools import setup
-
-setup(
-    pbr=True,
-)
+# Required for editable installs
+# https://discuss.python.org/t/specification-of-editable-installation/1564/
+import setuptools; setuptools.setup()
