bf8574cc32da4400d06e377fbc0dc0443f255b92
mesca <github@clisson.org>
2020-05-14 20:15:02 +0200

    Use setuptools-scm versioning

--- timeflux/__init__.py
+++ timeflux/__init__.py
@@ -4,6 +4,13 @@
 import os
 os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
-# Versinoning
-from pbr.version import VersionInfo
-__version__ = VersionInfo(__name__).release_string()
+# Versioning
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root='..', relative_to=__file__)
+except:
+    try:
+        from .version import version
+        __version__ = version
+    except:
+        __version__ = '0.0.0'
