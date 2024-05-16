da7dc02c274ce4442382fb5e752157d94d37714c
JulesGL <jules.gomel@outlook.fr>
2024-05-16 15:03:10 +0200

    Modified estimator attributes to correspond to argument for sklearn Pipeline class compatibility on instantiation

--- timeflux/estimators/transformers/shape.py
+++ timeflux/estimators/transformers/shape.py
@@ -4,13 +4,13 @@ from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 
 class Transpose(BaseEstimator, TransformerMixin):
     def __init__(self, axes):
-        self._axes = axes
+        self.axes = axes
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        return np.transpose(X, self._axes)
+        return np.transpose(X, self.axes)
 
     def fit_transform(self, X, y=None):
         return self.fit(X).transform(X)
@@ -18,8 +18,8 @@ class Transpose(BaseEstimator, TransformerMixin):
 
 class Expand(BaseEstimator, TransformerMixin):
     def __init__(self, axis=0, dimensions=3):
-        self._axis = axis
-        self._dimensions = dimensions
+        self.axis = axis
+        self.dimensions = dimensions
 
     def fit(self, X, y=None):
         return self
@@ -27,7 +27,7 @@ class Expand(BaseEstimator, TransformerMixin):
     def transform(self, X):
         X = np.asarray(X)
         if len(X.shape) < self._dimensions:
-            return np.expand_dims(X, axis=self._axis)
+            return np.expand_dims(X, axis=self.axis)
         else:
             return X
 
@@ -37,7 +37,7 @@ class Expand(BaseEstimator, TransformerMixin):
 
 class Reduce(BaseEstimator, TransformerMixin):
     def __init__(self, axis=0):
-        self._axis = axis
+        self.axis = axis
 
     def fit(self, X, y=None):
         return self
@@ -46,7 +46,7 @@ class Reduce(BaseEstimator, TransformerMixin):
         X = np.asarray(X)
         if X.ndim < 3:
             return X
-        return np.squeeze(X, axis=self._axis)
+        return np.squeeze(X, axis=self.axis)
 
     def fit_transform(self, X, y=None):
         return self.fit(X).transform(X)
